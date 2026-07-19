53b572fd004ef6bc8eb7be78ae2e7b8985d6b293
Omiisama <ompayalsony2@gmail.com>
2026-07-19 11:52:36 +0530

    Add files via upload

---
+++ 0. Template.cpp
@@ -0,0 +1,46 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpi = vector<pii>;
+using vpl = vector<pll>;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define F first
+#define S second
+
+const ll INF = (ll)4e18;
+const int MOD = 1e9+7;
+// const int MOD = 998244353;
+
+int dx[4]={1,0,0,-1};
+int dy[4]={0,-1,1,0};
+char dir[4]={'D', 'L', 'R', 'U'};
+
+void solve(){
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T=1;
+    cin>>T;
+
+    while(T--)
+        solve();
+}
